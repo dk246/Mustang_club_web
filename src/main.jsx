3a7e048a5d948ceaf68a56650c0503b83cfb1dd8
dk246 <76243568+dk246@users.noreply.github.com>
2025-08-30 14:03:54 +0530

    dep1

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,13 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
-import App from "./App";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <BrowserRouter basename="/Mustang_club_web">
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <BrowserRouter>
       <App />
     </BrowserRouter>
-  </React.StrictMode>
+  </StrictMode>
 );
