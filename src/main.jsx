e7ba19d11296e4c4986b2e0f4021637ccd8dc7bf
dk246 <76243568+dk246@users.noreply.github.com>
2025-08-30 14:08:46 +0530

    dep3

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import { BrowserRouter } from "react-router-dom";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/Mustang_club_web">
       <App />
     </BrowserRouter>
   </StrictMode>
